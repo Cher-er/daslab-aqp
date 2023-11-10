e2eb522376bb4d3b61ebb0302c9524919900d3d6
cher <cher18168402859@163.com>
2023-11-10 12:43:31 +0800

    update

--- do.py
+++ do.py
@@ -2,6 +2,7 @@ import json
 import vae.train
 import vae.score
 import os
+import cvae.data.prepare_data
 
 if __name__ == '__main__':
     with open(os.path.join(os.getcwd(), 'config', 'main.json')) as f:
@@ -11,3 +12,5 @@ if __name__ == '__main__':
         vae.train.train_vae()
     if args['vae']['gen']:
         vae.score.gen_sample()
+    if args['cvae']['train']:
+        cvae.data.prepare_data.prepare_data()
