d43f14e334f4bd633e2d5f989554a7231f69c316
cher <cher18168402859@163.com>
2023-11-12 12:00:16 +0800

    update

--- cvae/data/prepare_data.py
+++ cvae/data/prepare_data.py
@@ -37,8 +37,8 @@ def loader():
     one_hot_map = {}
     one_hot_max_sizes = []
     for i, cat_col in enumerate(cat_cols):
-        print(type(cat_col))
         unique_values.append(data[cat_col].unique())
+        print(type(unique_values[i][0]))
         one_hot_map[cat_col] = {x: i for i, x in enumerate(unique_values[i])}
         data[cat_col] = data[cat_col].map(one_hot_map[cat_col])
         one_hot_max_sizes.append(len(one_hot_map[cat_col]) - 1)
