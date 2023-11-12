3c0a0f5149864c4f9ff70d4ab99107e177bcb972
cher <cher18168402859@163.com>
2023-11-12 12:04:18 +0800

    update

--- cvae/data/prepare_data.py
+++ cvae/data/prepare_data.py
@@ -37,9 +37,7 @@ def loader():
     one_hot_map = {}
     one_hot_max_sizes = []
     for i, cat_col in enumerate(cat_cols):
-        unique_values.append(data[cat_col].unique())
-        if type(unique_values[i][0]) == np.int64:
-            one_hot_map[cat_col] = {int(x): i for i, x in enumerate(unique_values[i])}
+        unique_values.append(data[cat_col].unique().astype(str))
         one_hot_map[cat_col] = {x: i for i, x in enumerate(unique_values[i])}
         data[cat_col] = data[cat_col].map(one_hot_map[cat_col])
         one_hot_max_sizes.append(len(one_hot_map[cat_col]) - 1)
