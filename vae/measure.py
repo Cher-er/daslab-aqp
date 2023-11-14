3b46a367ee2ce0104636020a9088c08416920240
cher <cher18168402859@163.com>
2023-11-14 18:56:08 +0800

    update

--- vae/measure.py
+++ vae/measure.py
@@ -15,6 +15,6 @@ def sMAPE(truth, aqp):
 
 def measure():
     config = VAEConfig().get_config()
-    aqp = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples'])))
-    truth = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_truth.csv'.format(config['num_samples'])))
+    aqp = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples'])), header=False)
+    truth = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_truth.csv'.format(config['num_samples'])), header=False)
     print(sMAPE(truth, aqp))
