f610b1c65fe747510303fa856acd95f0cf0f1dbe
cher <cher18168402859@163.com>
2023-11-14 20:40:13 +0800

    update

--- cvae/utils.py
+++ cvae/utils.py
@@ -1,20 +1,24 @@
 import pandas as pd
 from config.config import VAEConfig
+import os
+import json
 
 
-def execute_avg(data_path):
+def gen_masked_samples():
     config = VAEConfig().get_config()
-    data = pd.read_csv(data_path, delimiter=",")
     with open(config["sql_file"]) as f:
         sqls = f.readlines()
 
-    results = []
+    with open(os.path.join(config["output_dir"], "{}_info.json".format(config["model_name"]))) as f:
+        dataset_info = json.load(f)
+
+    columns = dataset_info["columns"]
+
+    masked_datas = []
     for sql in sqls:
-        data_c = data.copy()
-        # print("[SQL]: {}".format(sql))
+        masked_data = ["nan"] * len(columns)
+
         sql = sql.split(";")[0]
-        agg = sql.split("SELECT")[1].split("FROM")[0].strip()
-        agg = agg.split("(")[1].split(")")[0].strip()
         where = sql.split("WHERE")[1].strip()
         if "(" in where:
             where = where.split("(")[1]
@@ -31,9 +35,8 @@ def execute_avg(data_path):
             pre = predicate.split("=")[1].strip().strip("'").strip("\"")
             if pre.isdigit():
                 pre = int(pre)
-            # print("col: {}, pre: {}".format(col, pre))
-            data_c = data_c[data_c[col] == pre]
+            masked_data[masked_data.index(col)] = pre
+        masked_datas.append(masked_data)
 
-        results.append(data_c[agg].mean())
-
-    return results
+    masked_datas = pd.DataFrame(masked_datas)
+    masked_datas.to_csv(os.path.join(config["output_dir"], "{}_masked_for_sql.tsv".format(config["model_name"])), sep='\t', na_rep="nan", header=columns, index=False)
