ad290ba58f7260fc4e053e5d6295bb4f34a24182
cher <cher18168402859@163.com>
2023-11-14 16:09:29 +0800

    update

--- vae/aqp.py
+++ vae/aqp.py
@@ -8,5 +8,27 @@ def execute_avg():
     data = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}.csv'.format(config['num_samples'])), delimiter=",")
     with open(config["sql_file"]) as f:
         sqls = f.readlines()
-    print(data)
-    print(sqls)
+
+    results = []
+    for sql in sqls:
+        agg = sql.split("SELECT")[1].split("FROM")[0].strip()
+        agg = agg.split("(")[1].split(")")[0].strip()
+        where = sql.split("WHERE")[1].strip()
+        if "(" in where:
+            where = where.split("(")[1]
+            where = where.split(")")[0].strip()
+        if "AND" in where:
+            predicates = where.split("AND")
+            for n, predicate in enumerate(predicates):
+                predicates[n] = predicate.strip()
+        else:
+            predicates = [where]
+
+        for predicate in predicates:
+            col = predicate.split("=")[0].strip()
+            pre = predicate.split("=")[1].strip()
+            data = data[data[col] == pre]
+
+        results.append(data[agg].mean())
+
+    print(results)
