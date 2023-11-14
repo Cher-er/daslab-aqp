7c26b9ea185715aa89d406916c164f09cf7dde06
cher <cher18168402859@163.com>
2023-11-14 18:14:28 +0800

    update

--- vae/aqp.py
+++ vae/aqp.py
@@ -7,6 +7,6 @@ import pickle
 def aqp_avg():
     config = VAEConfig().get_config()
     results = execute_avg(os.path.join(config["output_dir"], 'samples_{}.csv'.format(config['num_samples'])))
-    with open(os.path.join(config["output_dir"], 'samples_{}_aqp.pkl'.format(config['num_samples'])), 'w') as f:
+    with open(os.path.join(config["output_dir"], 'samples_{}_aqp.pkl'.format(config['num_samples'])), 'wb') as f:
         pickle.dump(results, f)
     print("AQP results has been saved in {}".format(os.path.join(config["output_dir"], 'samples_{}_aqp.pkl'.format(config['num_samples']))))
--- vae/ground_truth.py
+++ vae/ground_truth.py
@@ -7,6 +7,6 @@ import pickle
 def exact_avg():
     config = VAEConfig().get_config()
     results = execute_avg(config["input_file"])
-    with open(os.path.join(config["output_dir"], 'samples_{}_truth.pkl'.format(config['num_samples'])), 'w') as f:
+    with open(os.path.join(config["output_dir"], 'samples_{}_truth.pkl'.format(config['num_samples'])), 'wb') as f:
         pickle.dump(results, f)
     print("Ground truth has been saved in {}".format(os.path.join(config["output_dir"], 'samples_{}_truth.pkl'.format(config['num_samples']))))
