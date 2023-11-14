a9a778c1874542f2024b49f930c2e06200931df1
cher <cher18168402859@163.com>
2023-11-14 18:52:52 +0800

    update

--- vae/measure.py
+++ vae/measure.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import os
 from config.config import VAEConfig
+import numpy as np
+
+
+def sMAPE(truth, aqp):
+    result = 2 * (truth - aqp).abs() / (truth.abs() + aqp.abs())
+    result[np.isinf(result)] = 0
+    return result.mean()
 
 
 def measure():
     config = VAEConfig().get_config()
     aqp = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples'])))
     truth = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_truth.csv'.format(config['num_samples'])))
-    print(aqp)
-    print(truth)
+    print(sMAPE(truth, aqp))
