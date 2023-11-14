0c6a11b9abe82b60078320e5d94fbc942e9de5d8
cher <cher18168402859@163.com>
2023-11-14 19:49:19 +0800

    update

--- vae/measure.py
+++ vae/measure.py
@@ -15,6 +15,7 @@ def measure():
     aqp = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples'])), header=None)
     truth = pd.read_csv(os.path.join(config["output_dir"], 'samples_{}_truth.csv'.format(config['num_samples'])), header=None)
     smape = sMAPE(truth, aqp)
-    with open(os.path.join(config["output_dir"], 'samples_{}_measure.csv'.format(config['num_samples'])), 'w') as f:
-        f.write(str(smape))
+    smape.to_csv(os.path.join(config["output_dir"], 'samples_{}_measure.csv'.format(config['num_samples'])))
+    # with open(os.path.join(config["output_dir"], 'samples_{}_measure.csv'.format(config['num_samples'])), 'w') as f:
+    #     f.write(str(smape[0][0]))
     print("Measure result has been saved in {}".format(os.path.join(config["output_dir"], 'samples_{}_measure.csv'.format(config['num_samples']))))
