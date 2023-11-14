23a1fda12ed0fb18a80ea50addf156f727d50fe5
cher <cher18168402859@163.com>
2023-11-14 18:10:18 +0800

    update

--- vae/aqp.py
+++ vae/aqp.py
@@ -6,6 +6,6 @@ from vae.utils import execute_avg
 def aqp_avg():
     config = VAEConfig().get_config()
     results = execute_avg(os.path.join(config["output_dir"], 'samples_{}.csv'.format(config['num_samples'])))
-    with open(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples']))) as f:
+    with open(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples'])), 'w') as f:
         f.writelines(results)
     print("AQP results has been saved in {}".format(os.path.join(config["output_dir"], 'samples_{}_aqp.csv'.format(config['num_samples']))))
