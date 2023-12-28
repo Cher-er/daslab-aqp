7c58c886e4d13d980724c1dc7eca46a98b42e1b5
cher <cher18168402859@163.com>
2023-12-28 13:46:57 +0800

    update

--- sampling/random_sampling.py
+++ sampling/random_sampling.py
@@ -84,12 +84,12 @@ def exact():
             match = re.search(r'AVG\(([^)]+)\)', command, re.IGNORECASE)
             if match:
                 target = match.group(1)
-                results.append(sample_df[target].mean())
+                results.append(float(sample_df[target].mean()))
         elif agg == 'sum':
             match = re.search(r'SUM\(([^)]+)\)', command, re.IGNORECASE)
             if match:
                 target = match.group(1)
-                results.append(sample_df[target].sum() * (100 / sample_size))
+                results.append(float(sample_df[target].sum()) * (100 / sample_size))
 
     output_file = os.path.join(output_dir, f'random_sampling_{sample_size}.csv')
     with open(output_file, 'w') as f:
