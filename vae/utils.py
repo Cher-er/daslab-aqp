53de211d55a9662de9dbe767545baf0bbf901c18
cher <cher18168402859@163.com>
2023-12-29 10:21:45 +0800

    update

--- vae/utils.py
+++ vae/utils.py
@@ -45,6 +45,7 @@ def execute_avg(data_path):
             if col.endswith(('_c', '_n')):
                 col = col[:-2]
             data_c = data_c[data_c[col] == pre]
+            print(data_c)
         # print(data_c)
 
         results.append(data_c[agg].mean())
