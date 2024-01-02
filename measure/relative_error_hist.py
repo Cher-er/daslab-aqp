9000b4991a83e8be59a45f35dc9f2848865cba6b
cher <cher18168402859@163.com>
2024-01-02 10:59:33 +0800

    update

--- measure/relative_error_hist.py
+++ measure/relative_error_hist.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.array([1, 2, 3, 4, 5, 6])
-y1 = np.array([2, 4, 6])
-y2 = np.array([1, 2, 1])
 
 with open('ground_truth.csv') as f:
     ground_truth = np.array([line.strip() for line in f.readlines()]).astype(float)
@@ -16,10 +14,15 @@ with open('samples_10000_aqp.csv') as f:
     vae_10000_10 = np.array([line.strip() for line in f.readlines()]).astype(float)
     vae_10000_10 = np.abs(vae_10000_10 - ground_truth) / ground_truth
 
+with open('samples_100000_aqp.csv') as f:
+    vae_100000_20 = np.array([line.strip() for line in f.readlines()]).astype(float)
+    vae_100000_20 = np.abs(vae_100000_20 - ground_truth) / ground_truth
+
 bar_width = 0.3
 
-plt.bar(x - bar_width/2, random_sampling_0_1, label='Random Sampling', alpha=0.7, width=bar_width)
-plt.bar(x + bar_width/2, vae_10000_10, label='VAE_10000_10', alpha=0.7, width=bar_width)
+plt.bar(x - bar_width, random_sampling_0_1, label='Random Sampling', alpha=0.7, width=bar_width)
+plt.bar(x, vae_10000_10, label='VAE_10000_10', alpha=0.7, width=bar_width)
+plt.bar(x + bar_width, vae_100000_20, label='VAE_100000_20', alpha=0.7, width=bar_width)
 
 plt.legend()
 
