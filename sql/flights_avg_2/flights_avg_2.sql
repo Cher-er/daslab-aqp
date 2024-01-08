778bc54c76fd1c8f23e9be5b4438ca7437cf4e37
cher <cher18168402859@163.com>
2024-01-08 13:57:59 +0800

    update

--- sql/flights_avg_2/flights_avg_2.sql
+++
@@ -1,6 +0,0 @@
-select avg(dep_delay) from flights where year_data = 2000;
-select avg(taxi_out) from flights where unique_carrier = '9E';
-select avg(taxi_in) from flights where origin = 'MSP';
-select avg(arr_delay) from flights where origin_state_abr = 'TX';
-select avg(air_time) from flights where dest = 'BTV';
-select avg(distance) from flights where dest_state_abr = 'MO';
--- sql/flights_count_2/flights_count_2.sql
+++
@@ -1,6 +0,0 @@
-select count(*) from flights where year_data = 2000;
-select count(*) from flights where unique_carrier = '9E';
-select count(*) from flights where origin = 'MSP';
-select count(*) from flights where origin_state_abr = 'TX';
-select count(*) from flights where dest = 'BTV';
-select count(*) from flights where dest_state_abr = 'MO';
